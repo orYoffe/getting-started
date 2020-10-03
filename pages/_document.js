fe0d1bd40cfe7411eaf062fcb778b6e979dea705
orYoffe <ragiop07@gmail.com>
2020-10-03 18:15:28 +0200

    fixed GA scripts

--- pages/_document.js
+++ pages/_document.js
@@ -12,22 +12,22 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-<script>
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-179549639-1');
-</script>`,
+`,
             }}
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5JX7WB9');</script>`,
+})(window,document,'script','dataLayer','GTM-5JX7WB9');`,
             }}
           />
         </Head>
