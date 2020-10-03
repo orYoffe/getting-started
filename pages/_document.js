aafac22b5656d66435a345f78cb3ef12da9b2934
orYoffe <ragiop07@gmail.com>
2020-10-03 18:09:01 +0200

    added GA

---
+++ pages/_document.js
@@ -0,0 +1,54 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang={this.props.lang || "en"}>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-179549639-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-179549639-1');
+</script>`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JX7WB9');</script>`,
+            }}
+          />
+        </Head>
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5JX7WB9"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>`,
+            }}
+          ></noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
